f922c604449b1ab9939ad7559c19b5c42c8fd1d4
Vijaya Sekhar Gullapalli <sekhargullapalli@gmail.com>
2023-11-27 00:09:01 +0100

    add the Amazon s3 .net utilities and update gitignore for config.sj

---
+++ src/dotnet/Utilities/AWS-s3-Reader/Program.cs
@@ -0,0 +1,43 @@
+﻿using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+namespace S3TestApp;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var unsigned = new AnonymousAWSCredentials();
+        var client = new AmazonS3Client(unsigned, Amazon.RegionEndpoint.USEast1);
+        var bucketName = "ai-for-life-sciences-2";
+        var listRequest = new ListObjectsRequest
+        {
+            BucketName = bucketName,
+            Delimiter = "/",
+        };
+        ListObjectsResponse listResponse;
+        int count = 0;
+        using TextWriter textWriter = new StreamWriter($"{bucketName}-contents.txt");
+        do
+        {
+            listResponse = await client.ListObjectsAsync(listRequest);
+            foreach (var obj in listResponse.CommonPrefixes)
+            {                
+                listRequest.Prefix = obj;
+                var sublistResponse = await client.ListObjectsAsync(listRequest);
+                foreach (var subobj in sublistResponse.S3Objects)
+                {
+                    textWriter.WriteLine(subobj.Key);
+                    Console.WriteLine((++count) + " {0} {1}", subobj.Size, subobj.Key);
+                }
+            }
+            foreach (var obj in listResponse.S3Objects)
+            {
+                textWriter.WriteLine(obj.Key);
+                Console.WriteLine((++count) + " {0} {1}", obj.Size, obj.Key);
+            }
+            listRequest.Marker = listResponse.NextMarker;
+        } while (listResponse.IsTruncated);
+
+    }
+}
